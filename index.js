317bfa9208b5319b4e4f7e9a67af5fa190f17a26
Wang Zhongliang <wangz@garena.com>
2017-02-07 12:23:32 +0800

    Add an option to disable this addon

--- index.js
+++ index.js
@@ -4,8 +4,16 @@ var VendorDedupe = require('./lib/VendorDedupe');
 
 module.exports = {
   name: 'ember-engines-dedupe-vendor',
-  isDevelopingAddon: function() {
-    return true;
+  isEnabled: function() {
+    var enabled = true;
+    var _app = this.registry.app;
+    while (_app.app) {
+      _app = _app.app;
+    }
+    if (_app.options && _app.options['ember-engines-dedupe-vendor'] && _app.options['ember-engines-dedupe-vendor'].disabled) {
+      enabled = false;
+    }
+    return enabled;
   },
   postprocessTree: function(type, tree) {
     if (type === 'all') {
